78fb85e736ae0bb0467a67c5aee2e2f677cbfedb
yminery <91838072+yminery@users.noreply.github.com>
2021-12-05 19:11:30 +0200

    Add files via upload

--- notes.py
+++ notes.py
@@ -1,6 +1,18 @@
 from PyQt5.QtWidgets import (QApplication, QWidget, QListWidget, QPushButton,
-QTextEdit, QLineEdit, QLabel, QVBoxLayout, QHBoxLayout)
+QTextEdit, QLineEdit, QLabel, QVBoxLayout, QHBoxLayout, QInputDialog)
 
+import json
+
+
+notes = {"Hello" : {
+        "txt" : "This is the best note in the world!!!",
+        "tags" : ['welcome', 'instruct']
+    }
+}   
+'''
+with open('notes_data.json', 'w') as file:
+    json.dump(notes, file)
+'''
 app = QApplication([])
 notes_win = QWidget()
 notes_win.setWindowTitle('smart notes')
@@ -51,5 +63,88 @@ col_2.addLayout(row_4)
 layout_notes.addLayout(col_1, stretch = 2)
 layout_notes.addLayout(col_2, stretch = 1)
 notes_win.setLayout(layout_notes)
+
+def show_note():
+    key = list_notes.selectedItems()[0].text()
+    field_txt.setText(notes[key]['txt'])
+    list_tags.clear()
+    list_tags.addItems(notes[key]['tags'])
+
+def add_note():
+    note_name, ok = QInputDialog.getText(notes_win, 'Add note', 'name of note')
+    if ok and note_name:
+        notes[note_name] = {'txt' : "", "tags": []}
+        list_notes.addItem(note_name)
+
+def save_note():
+    if list_notes.selectedItems():
+        key = list_notes.selectedItems()[0].text()
+        notes[key]['txt'] = field_txt.toPlainText()
+        with open('notes_data.json', 'w') as file:
+            json.dump(notes, file, sort_keys=True)
+
+def del_note():
+    if list_notes.selectedItems():
+        key = list_notes.selectedItems()[0].text()
+        del notes[key]
+        list_notes.clear()
+        list_tags.clear()
+        field_txt.clear()
+        list_notes.addItems(notes)
+        with open('notes_data.json', 'w') as file:
+            json.dump(notes, file, sort_keys=True)
+
+def add_tags():
+    if list_notes.selectedItems():
+        key = list_notes.selectedItems()[0].text() 
+        tag = field_tag.text()
+        if not tag in notes[key]['tags']:
+            notes[key]['tags'].append(tag)
+            list_tags.addItem(tag)
+            field_tag.clear()
+            with open('notes_data.json', 'w') as file:
+                json.dump(notes, file, sort_keys=True)
+
+def del_tag():
+    if list_notes.selectedItems():
+        key = list_notes.selectedItems()[0].text() 
+        tag = list_tags.selectedItems()[0].text()
+        notes[key]['теги'].remove(tag)
+        list_tags.clear()
+        list_tags.addItems(notes[key]['теги'])
+        with open('notes_data.json', 'w') as file:
+                json.dump(notes, file, sort_keys=True)
+
+def search_tag():
+    tag = field_tag.text()
+    if but_tag_sher.text() == 'Поиск по тегу' and tag:
+        notes_filtered = {}
+        for note in notes:
+            if tag in notes[note]['теги']:
+                notes_filtered[note] = notes[note]
+        but_tag_sher.setText('Сбросить поиск')
+        list_notes.create()
+        list_tags.clear()
+        list_notes.addItems(notes_filtered)
+    elif but_tag_sher.text() == 'Сбросить поиск':
+        list_notes.clear()
+        field_tag.clear()
+        list_tags.clear()
+        list_notes.addItems(notes)
+        but_tag_sher.setText('Поиск по тегу')
+
+but_tag_sher.clicked.connect(search_tag)
+
+but_tag_de.clicked.connect(add_tags)                            
+but_tag_dd.clicked.connect(add_tags)                            
+but_not_del.clicked.connect(del_note)            
+but_not_save.clicked.connect(save_note)       
+but_not_create.clicked.connect(add_note)
+list_notes.itemClicked.connect(show_note)
+
+with open ('notes_data.json', 'r') as file:
+    notes = json.load(file)
+    
+list_notes.addItems(notes)
 notes_win.show()
 app.exec_()
